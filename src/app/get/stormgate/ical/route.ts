a98bbd092a38229602b025b391e777a123fae9c3
Juho Nieminen <nieminen.juho@gmail.com>
2024-08-15 01:16:46 +0300

    Fix unfinished stormgate ical route

--- src/app/get/stormgate/ical/route.ts
+++ src/app/get/stormgate/ical/route.ts
@@ -3,7 +3,9 @@ import { getMatches } from '../matches';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
+	// TODO: stormgate iCal
 	// const matches = await getMatches(request);
 	// const ical = createMatchEvents(matches);
-	// return new NextResponse<string>(ical, { status: 200 });
+	const ical = 'BEGIN:VCALENDAR\nEND:VCALENDAR';
+	return new NextResponse<string>(ical, { status: 200 });
 }
