f7b0aea41a8c7c6a0ae18515f947ff6d8d5e9feb
Juho Nieminen <juho.nieminen@fraktio.fi>
2025-07-21 14:58:26 +0300

    wip: use wiki lua script to get matches html

--- src/liquipedia/fetch-page.ts
+++ src/liquipedia/fetch-page.ts
@@ -7,6 +7,13 @@ export const fetchWikiContent = async (page: string, wikiRoot: string): Promise<
 	return json.query?.pages[0]?.revisions[0]?.slots?.main?.content ?? '';
 };
 
+export const fetchWikiLuaContent = async (endpoint: string, wikiRoot: string): Promise<string> => {
+	const url = `${wikiRoot}${endpoint}`;
+	const data = await fetch(url, { next: { revalidate: minutesInSeconds(5) } });
+	const json = await data.json();
+	return json.query?.pages[0]?.revisions[0]?.slots?.main?.content ?? '';
+};
+
 export const fetchWikiParsed = async (page: string, wikiRoot: string): Promise<string> => {
 	const url = `${wikiRoot}/api.php?action=parse&page=${page}&format=json&prop=text`;
 	const data = await fetch(url, { next: { revalidate: minutesInSeconds(5) } });
--- src/liquipedia/starcraft2/extract-matches.ts
+++ src/liquipedia/starcraft2/extract-matches.ts
@@ -1,18 +1,45 @@
-import { fetchWikiContent } from '../fetch-page';
+import { fetchWikiLuaContent } from '../fetch-page';
 import { wikitextToArray } from '../parse-wikitext';
 import { SC2Match } from '@/@types/starcraft';
 import { MatchParserStarCraft2 } from './match-parser';
 import { cache } from 'react';
 import { config } from '@/config';
 
-const MATCHES_PAGE_SC2 = 'Liquipedia:Upcoming_and_ongoing_matches';
-
 export const revalidate = 300;
 
 export const extractStarCraft2Matches = cache(async (): Promise<SC2Match[]> => {
-	const wikitext = await fetchWikiContent(MATCHES_PAGE_SC2, config.sc2WikiRootUrl);
+	const matchesEndpoint = getSc2MatchesEndpoint();
+	const wikitext = await fetchWikiLuaContent(matchesEndpoint, config.sc2WikiRootUrl);
 	const wikiArray = wikitextToArray(wikitext);
 	const matches = new MatchParserStarCraft2().parseMatches(wikiArray);
 
 	return matches;
 });
+
+function getSc2MatchesEndpoint() {
+	const baseUrl = '/api.php';
+
+	const params = new URLSearchParams({
+		action: 'parse',
+		format: 'json',
+		contentmodel: 'wikitext',
+		maxage: '600',
+		smaxage: '600',
+		disablelimitreport: 'true',
+		uselang: 'content',
+		prop: 'text',
+		text: `{{#invoke:Lua|invoke
+    | module=MatchTicker/Custom
+    | fn=newMainPage
+    | dev=false
+    | type=recent
+    | limit=30
+    | filterbuttons-liquipediatier=1,2,3,4,-1
+    | filterbuttons-liquipediatiertype=monthly,weekly,biweekly,showmatch,daily,archon,ffa,4v4,1v2,3v3,2v2,qualifier,charity
+  }}`.replace(/\s+/g, ' '),
+	});
+
+	const url = `${baseUrl}?${params.toString()}`;
+
+	return url;
+}
